c299744211ab204a0880692e2f52950e33fa94bb
wesleysilva77 <wesleyyurisilva@gmail.com>
2025-11-13 13:03:39 -0300

    feat: adicionar funções da lista

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 /**** Escreva o código abaixo 👇******/
 
+function somar(a, b) {
+    return a + b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function dividir(a, b) {
+    if (b === 0) {
+        throw new Error("Erro: divisão por zero");
+    }
+    return a / b;
+}
+
+function calcularMedia(nota1, nota2, nota3, nota4) {
+    const soma = nota1 + nota2 + nota3 + nota4;
+    return soma / 4;
+}
 
 
+function elevarPotencia(base, expoente) {
+    return Math.pow(base, expoente);
+} 
 
+function calcularAreaRetangulo(largura, altura) {
+    return largura * altura;
+} 
 
+function verificarParOuImpar(numero) {
+    return numero % 2 === 0 ? "par" : "ímpar";
+} 
 
+function calcularRaizQuadrada(numero) {
+    if (numero < 0) {
+        return "Erro: número negativo";
+    }
+    return Math.sqrt(numero);
+}
+
+function calcularHipotenusa(catetoA, catetoB) {
+    return Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
+}
 
 
 /**** Escreva o código acima ☝️******/
